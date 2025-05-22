a47e35019d82a1eeacffc23fd145cf74f5f1a64c
miield <aodasola95@gmail.com>
2025-05-22 03:23:07 +0100

    chore: add TaskDelegationInput struct to the Task dto

--- dto/task.go
+++ dto/task.go
@@ -6,3 +6,9 @@ type Task struct {
 	Description string `json:"description" binding:"required"`
 	Completed 	string `json:"completed"`
 }
+
+type TaskDelegationInput struct {
+	DelegateeID     string `json:"delegatee_id" binding:"required"`
+	Permission string `json:"permission" binding:"required"` // expects "R", or "U" in this context
+}
+
