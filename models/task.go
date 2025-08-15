e5f4fd7159ac7cb0abf8cbc75f6f2a9674f0d75f
miield <aodasola95@gmail.com>
2025-08-15 21:28:16 +0100

    chore: clear up the comment

--- models/task.go
+++ models/task.go
@@ -9,8 +9,7 @@ import (
 
 // GORM
 type Task struct {
-	// json is in the go application while the db i s mapped to it in the database
-	// ID			uuid.UUID `gorm:"type:uuid;default:uuid_default_generate_v4();"primaryKey" json:"id"` 
+	// json is in the go application while the db is mapped to it in the database
 	ID			string 		`gorm:"primaryKey" json:"id"`
 	Title 		string 		`gorm:"not null" json:"title"`
 	Description	string 		`gorm:"not null" json:"description"`
@@ -26,4 +25,3 @@ func (t *Task) BeforeCreate(tx *gorm.DB) error {
 	}	
 	return nil
 }
-
