25125aea402b59003e81ff7cbd3b8d91aff81bd9
miield <aodasola95@gmail.com>
2025-08-15 21:01:11 +0100

    chore: cleaned up the dto files

--- dto/task.go
+++ dto/task.go
@@ -1,6 +1,6 @@
 package dto
 
-// this is an abstract/interface of the real model validates payloads
+// this is an abstract/interface of the real model that validates payloads
 type Task struct {
 	Title 		string `json:"title" binding:"required"`
 	Description string `json:"description" binding:"required"`
@@ -11,4 +11,3 @@ type TaskDelegationInput struct {
 	DelegateeID     string `json:"delegatee_id" binding:"required"`
 	Permission 		string `json:"permission" binding:"required"` // expects "R", or "U" , "O" in this context
 }
-
