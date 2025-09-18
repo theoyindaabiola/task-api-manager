f6b97da6b3a716e091eb5f8cb82753ce81e23490
miield <aodasola95@gmail.com>
2025-09-18 19:45:05 +0100

    feat(dto): add request/response struct for otp request, otp verification, and user response

--- dto/user.go
+++ dto/user.go
@@ -16,6 +16,29 @@ type ForgotPasswordDTO struct {
 }
 
 type ResetPasswordDTO struct {
-	Token 		string `Json:"token" binding:"required"`
-	Password	string `json:"password" binding:"required"`
+	Token 			string `json:"token" binding:"required"`
+	Password		string `json:"password" binding:"required"`
+}
+
+type Toggle2FARequest struct {
+	Enabled 		bool `json:"enabled"`
+}
+
+type Toggle2FAResponse struct {
+    UserID     		string `json:"user_id"`
+    Enabled2FA 		bool   `json:"enabled_2fa"`
+    Message    		string `json:"message"`
+	Token			string `json:"token"`
+}
+
+type RequestOtpDTO struct {
+	Email 			string `json:"email" binding:"required"`
+}
+
+type VerifyOtpDTO struct {
+	OTP				string `json:"otp" binding:"required"`
+}
+
+type UserResponseDTO struct {
+	IsOtpVerified	bool `json:"is_otp_verified"`
 }
