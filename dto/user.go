a92c2719d31dc410fd54a5794cfeeaf2a1101768
miield <aodasola95@gmail.com>
2025-09-18 19:48:03 +0100

    style(dto): adjust spacing in RegisterUserDTO, LoginUserDTO, and ForgotPasswordDTO

--- dto/user.go
+++ dto/user.go
@@ -1,18 +1,18 @@
 package dto
 
 type RegisterUserDTO struct {
-	Username 	string `Json:"username" binding:"required"`
-	Email 		string `Json:"email" binding:"required"`
-	Password	string `json:"password" binding:"required"`
+	Username 		string `json:"username" binding:"required"`
+	Email 			string `json:"email" binding:"required"`
+	Password		string `json:"password" binding:"required"`
 }
 
 type LoginUserDTO struct {
-	Username 	string `Json:"username" binding:"required"`
-	Password	string `json:"password" binding:"required"`
+	Username 		string `json:"username" binding:"required"`
+	Password		string `json:"password" binding:"required"`
 }
 
 type ForgotPasswordDTO struct {
-	Email 		string `Json:"email" binding:"required"`
+	Email 			string `json:"email" binding:"required"`
 }
 
 type ResetPasswordDTO struct {
