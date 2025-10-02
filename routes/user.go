bc27ba09fc6d82588796d9d287ef2a9b4e9a45a3
miield <aodasola95@gmail.com>
2025-10-02 05:40:34 +0100

    refactor(user-routes): integrated SMS 2FA paths and userService into user routes

--- routes/user.go
+++ routes/user.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"taskapi/controllers"
 	"taskapi/middleware"
+	"taskapi/services"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +12,7 @@ type UserRoutes struct {
 	UserController *controllers.UserController
 }
 
-func RegisterUserRoutes(router *gin.Engine, userController *controllers.UserController) {
+func RegisterUserRoutes(router *gin.Engine, userController *controllers.UserController, userService *services.UserService) {
 	// group the routes
 	userRoutes := router.Group("/api/users")
 	{
@@ -22,8 +23,9 @@ func RegisterUserRoutes(router *gin.Engine, userController *controllers.UserCont
 		userRoutes.POST("/reset-password", userController.ResetPassword)
 
 		// 2FA routes allowed in middleware
-		userRoutes.POST("/enable-totp", middleware.JWTAuthMiddleware(), userController.EnableTOTP)
-		userRoutes.POST("/verify-totp", middleware.JWTAuthMiddleware(), userController.VerifyTOTP)
-		userRoutes.POST("/disable-totp", middleware.JWTAuthMiddleware(), userController.DisableTOTP)
+		userRoutes.POST("/enable-sms", middleware.JWTAuthMiddleware(userService), userController.EnableSMS)
+		userRoutes.POST("/request-sms", middleware.JWTAuthMiddleware(userService), userController.RequestSMS)
+		userRoutes.POST("/verify-sms", middleware.JWTAuthMiddleware(userService), userController.VerifySMS)
+		userRoutes.POST("/disable-sms", middleware.JWTAuthMiddleware(userService), userController.DisableSMS)
 	}
 }
