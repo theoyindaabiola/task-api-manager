775305adf9a40aab013b5d79bdc753200c15d3dd
miield <aodasola95@gmail.com>
2026-04-07 20:31:38 +0100

    refactor: updates DTOs with validation and made it consistent

--- dto/user.go
+++ dto/user.go
@@ -2,7 +2,7 @@ package dto
 
 type RegisterUserDTO struct {
 	Username 		string `json:"username" binding:"required"`
-	Email 			string `json:"email" binding:"required"`
+	Email 			string `json:"email" binding:"required,email"`
 	Password		string `json:"password" binding:"required"`
 }
 
@@ -12,7 +12,7 @@ type LoginUserDTO struct {
 }
 
 type ForgotPasswordDTO struct {
-	Email 			string `json:"email" binding:"required"`
+	Email 			string `json:"email" binding:"required,email"`
 }
 
 type ResetPasswordDTO struct {
@@ -25,15 +25,16 @@ type VerifyTOTPDTO struct {
 }
 
 type RequestOtpDTO struct {
-	Email 			string `json:"email" binding:"required"`
+	Email 			string `json:"email" binding:"required,email"`
 }
 
 type VerifyOtpDTO struct {
-	OTP				string `json:"otp" binding:"required"`
+	Code				string `json:"code" binding:"required"`
 }
 
 type UserResponseDTO struct {
 	EmailOtpVerified	bool `json:"email_otp_verified"`
 	TOTPVerified		bool `json:"totp_verified"`
+	TwoFactorType    	string `json:"two_factor_type"`
 }
 
